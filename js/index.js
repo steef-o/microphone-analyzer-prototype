de6cea9b2da21a267ab73d8debe1da614e96ca75
Steffen Andre Hagen <steffen.hagen@knowit.no>
2021-10-04 19:33:57 +0200

    add files and gitignore file

---
+++ js/index.js
@@ -0,0 +1,40 @@
+let volume = 0;
+const player = document.querySelector('lottie-player');
+function onMicrophoneDenied() {
+    console.log('denied');
+}
+
+async function onMicrophoneGranted(stream) {
+    const audioContext = new window.AudioContext();
+
+    console.log('Setting up volume processor');
+
+    await audioContext.audioWorklet.addModule('js/volume-processor.js');
+
+    const mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+    const node = new AudioWorkletNode(audioContext, 'volume-processor');
+
+    node.port.onmessage = event => {
+        if (event.data.volume) {
+            volume = Math.ceil(event.data.volume * 100);
+        }
+    };
+
+    mediaStreamSource.connect(node).connect(audioContext.destination);
+
+    render();
+}
+
+function render() {
+    player.seek(`${volume}%`);
+    // console.log(`${volume * 1000}`);
+
+    requestAnimationFrame(render);
+}
+
+navigator.getUserMedia(
+    { audio: true },
+    onMicrophoneGranted,
+    onMicrophoneDenied
+);
